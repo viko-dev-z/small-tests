727761e5358fb3bbe656bd0c36044660f58dd657
Victor Paredes <vico.sistemas@gmail.com>
2018-07-06 22:46:41 -0400

    Adding FormBuilder for the communication

--- src/app/shared/second-modal/second-modal.component.ts
+++ src/app/shared/second-modal/second-modal.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-second-modal',
@@ -6,10 +8,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./second-modal.component.css']
 })
 export class SecondModalComponent implements OnInit {
+  closeResult: string;
+  inputsForm: FormGroup;
+  Spacing: number;
+  DLarge: number;
 
-  constructor() { }
+  constructor(private modalService: NgbModal, private fb: FormBuilder) {}
 
-  ngOnInit() {
+
+  ngOnInit(): void {
+    this.inputsForm = this.fb.group({
+      Spacing: [this.Spacing, []],
+      DLarge: [this.DLarge, []]
+    });
+  }
+
+  open(content) {
+    this.modalService.open(content).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
   }
 
 }
