4f6c89d18aff76a5601de37f8d942650eedf1825
Victor Paredes <vico.sistemas@gmail.com>
2018-07-06 21:42:01 -0400

    adding components and bootstrap 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ParentComponent } from './parent/parent/parent.component';
+import { ModalComponent } from './shared/modal/modal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ParentComponent,
+    ModalComponent
   ],
   imports: [
     BrowserModule
